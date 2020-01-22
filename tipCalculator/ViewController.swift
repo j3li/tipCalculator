1818cf8c5ce92d9c2b566a0e52a13568a8cbe783
Jing Li <j3li@ucsd.edu>
2020-01-21 21:39:48 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Jing Li on 1/21/20.
+//  Copyright © 2020 Jing Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
